5335e9a18ce9770ec2d987e3d7f8891aebce5726
friggito <ornella.friggit@gmail.com>
2019-05-22 12:41:26 -0400

    schema: set up models

--- db/migrate/20190522162717_initial_schema.rb
+++ db/migrate/20190522162717_initial_schema.rb
@@ -2,13 +2,23 @@ class InitialSchema < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.text :description
+      t.text   :description
       t.string :address1
       t.string :address2
       t.string :city
       t.string :state
-
+      t.string :zipcode
+      t.string :phone
+      t.string :email
     end
 
+    create_table :reviews do |t|
+      t.integer :rating
+      t.string  :comment
+    end
+
+    create_table :categories do |t|
+      t.string :name
+    end
   end
 end
